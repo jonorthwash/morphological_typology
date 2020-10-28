9f3567d9ea073a78f1fc9fef172a4c5df546e784
Francis Tyers <ftyers@prompsit.com>
2020-10-28 04:39:26 +0000

    updates

--- plotting.py
+++ plotting.py
@@ -1,9 +1,10 @@
+import sys
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("data.tsv", sep='\t') 
+df = pd.read_csv(sys.argv[1], sep='\t') 
 
-fig = px.scatter_3d(df, x='featspertokens', y='analysesperlength', z='analysespertypes', text='lang', opacity=0.7)
+fig = px.scatter_3d(df, y='featspertokens', z='analysesperlength', x='analysespertypes', text='lang', opacity=0.7)
 
 # tight layout
 #fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
